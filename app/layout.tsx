e46fb1c0a2f04b35629ea922ecd9c9bb0e351da5
Aravind Gunasekaran <aravindusa@yahoo.com>
2024-08-13 12:04:13 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/favicon.ico"/>
+      </head>
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}
       >
